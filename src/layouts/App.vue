4e37d0e1de18a0118eea8580931341ff44c32bdb
shicongrui <1102163949@qq.com>
2018-04-22 17:44:11 +0800

    项目结构 路由容器更改

---
+++ src/layouts/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <transition name="list" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    12
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  mounted () {
+    console.log(this.$store.state.index)
+  }
+}
+</script>
+<style>
+</style>
