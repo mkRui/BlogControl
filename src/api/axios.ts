2bd1131e5cc4007a8f0274d593b3f91a23ecefca
shicongrui <1102163949@qq.com>
2018-05-08 23:01:35 +0800

    接口更改 typescript 形式

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,12 +2,12 @@ import axios from 'axios'
 import * as config from './../config'
 import queryString from 'querystring'
 
-const ax = axios.create({
+const ax:any = axios.create({
   baseURL: config.contentPath,
   timeout: 5000
 })
 
-ax.interceptors.request.use(config => {
+ax.interceptors.request.use((config:any) => {
   if (config.method === 'post' ||
       config.method === 'put' ||
       config.method === 'delete' ||
@@ -16,7 +16,7 @@ ax.interceptors.request.use(config => {
     config.data = queryString.stringify(config.data)
   }
   return config
-}, error => {
+}, (error: any) => {
   return Promise.reject(error)
 })
 
