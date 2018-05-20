97db823fe136af33b5dd86235a144696ee96b531
shicongrui <1102163949@qq.com>
2018-05-20 16:12:44 +0800

    关于文章的筛选

---
+++ src/pages/viewArticle/components/articleState.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="stateBtn">
+    <p class="stateBar">
+      <span>文章状态:</span>
+      <span v-for="(item, index) in state" class="options" :class="index === select ? 'optionSelect' : ''" :key="index" @click="selection(item, index)">{{ item.content }}</span>
+    </p>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Vue, Emit, Watch } from 'vue-property-decorator'
+
+interface stateOption {
+  content: string,
+  state: number | string
+}
+
+@Component
+export default class stateBtn extends Vue {
+  private state: stateOption[] = [{content: '全部', state: ''}, {content: '发布', state: 1}, {content: '草稿', state: 0}]
+  private select: number = 0
+
+  private selection (item: stateOption, index: number) {
+    console.log(index)
+    this.select = index
+  }
+}
+</script>
+<style lang='scss' scoped>
+.stateBtn {
+  width: 100%;
+  border-bottom: 2px dashed $border;
+  padding: 10px 10px 10px 0px;
+  .stateBar {
+    padding: 6px 10px 6px 0px;
+    span {
+      color: $border;
+      &:first-child {
+        font-size: 16px;
+        margin-right: 10px;
+      }
+      &.options {
+        font-size: 15px;
+        color: $border;
+        padding:5px 20px;
+        margin-left: 15px;
+        border-radius: 5px;
+        box-sizing: border-box;
+        border: 2px solid $border;
+        &:hover {
+          background: $border;
+          color: #fff;
+        }
+      }
+      &.optionSelect {
+        background: $border;
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/viewArticle/components/tableList.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="tableContent">
+    <div class='tableList'>
+      <el-table
+        :data="articleList"
+        style="width: 100%">
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <ul class="details">
+              <li><span>文章说明：</span>{{ props.row.subTitle }}</li>
+              <li><span>文章标签：</span>{{ props.row.tag }}</li>
+              <li><span>文章浏览量：</span>{{ props.row.views }}</li>
+            </ul>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="文章标题"
+          prop="title">
+          <template slot-scope="props">
+            {{ props.row.title.length > 15 ? props.row.title.slice(0,15) + '...' : props.row.title }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="发布人"
+          prop="user">
+        </el-table-column>
+        <el-table-column
+          label="发布时间"
+          prop="date">
+        </el-table-column>
+        <el-table-column
+          label="所属标签"
+          prop="tag">
+        </el-table-column>
+        <el-table-column
+          width='230'
+          label="操作">
+          <template slot-scope="scope">
+            <el-button type='success' size="small">查看</el-button>
+            <el-button type='primary' size="small">编辑</el-button>
+            <el-button type='danger' size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class='pagination'>
+       <el-pagination
+        background
+        @current-change="handleCurrentChange"
+        :page-size="pageSize"
+        layout="prev, pager, next, jumper"
+        :total="totalPage">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+
+interface list {
+  title: string,
+  user: string,
+  date: number,
+  subTitle: string,
+  tag: string,
+  views: number
+}
+
+@Component
+export default class tableList extends Vue {
+  private articleList: list[] = [
+    {title: 'web前端进阶之路', user: 'qzuser', date: 1526198985076, subTitle: 'html5 + 迎接移动时代', tag: 'web前端', views: 20},
+    {title: 'python 与 人工智能', user: 'qzuser', date: 1526198985076, subTitle: 'html5 + 迎接移动时代', tag: 'web前端', views: 20},
+    {title: '波士顿机械狗', user: 'qzuser', date: 1526198985076, subTitle: 'html5 + 迎接移动时代', tag: 'web前端', views: 20},
+    {title: 'react native', user: 'qzuser', date: 1526198985076, subTitle: 'html5 + 迎接移动时代', tag: 'web前端', views: 20},
+    {title: 'react native', user: 'qzuser', date: 1526198985076, subTitle: 'html5 + 迎接移动时代', tag: 'web前端', views: 20}
+  ]
+  private pageSize: number = 5
+  private totalPage: number = 10
+
+  private handleCurrentChange (val: number):void {
+    console.log(val)
+  }
+}
+
+</script>
+<style lang='scss' scoped>
+.tableContent {
+  width: 100%;
+  .tableList {
+    background: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    overflow: auto;
+  }
+  .pagination {
+    margin-top: 20px;
+    display: flex;
+    justify-content: flex-end;
+  }
+  .details {
+    li {
+      line-height: 2;
+      span {
+        margin-right: 10px;
+      }
+    }
+  }
+}
+</style>
