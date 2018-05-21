e15d6757248fe9b17c83cb289b7d1d177b231de8
shicongrui <1102163949@qq.com>
2018-05-21 13:06:52 +0800

    增加 markdownUeditor  markdown 语法编辑器

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,16 @@ import './assets/scss/element-variables.scss'
 import store from './store/index'
 import { format } from './filter/index'
 
+// 以下都是simplemde的配置选项
+import VueSimplemde from 'vue-simplemde'
+import 'simplemde/dist/simplemde.min.css'
+import 'github-markdown-css'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/monokai-sublime.css'
+(window as any).hljs = hljs
+
+Vue.use(VueSimplemde)
+
 Vue.filter('format', format)
 
 Vue.use(ElementUI)
---
+++ src/pages/editorArticle/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="ts-Ueditor">
+    <div class="titleHeader">
+      <title-art></title-art>
+    </div>
+    <div class="options">
+      <screening></screening>
+      <uploader></uploader>
+    </div>
+    <div class="editorContent">
+      <markdown-editor v-model="ueditor" ref="VueUeditor" preview-class="markdown-body" :highlight="true"></markdown-editor>
+    </div>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import markdownEditor from 'vue-simplemde/src/markdown-editor.vue'
+import titleArt from './components/title.vue'
+import screening from './components/screening.vue'
+import uploader from './components/uploader.vue'
+
+@Component({
+  components: {
+    markdownEditor,
+    titleArt,
+    screening,
+    uploader
+  }
+})
+
+export default class TheEditor extends Vue {
+  private ueditor: string = ''
+
+  private handleInput () {
+    console.log(1)
+  }
+
+  private mounted ():void {
+    console.log(1)
+  }
+
+  @Watch('ueditor')
+  ueditorText (val: string) {
+    console.log(val)
+  }
+}
+
+</script>
+<style lang='scss' scoped>
+.ts-Ueditor {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  min-width: 1100px;
+  overflow: auto;
+  .titleHeader {
+    background: #fff;
+    padding: 20px 20px 5px 20px;
+    width: 100%;
+    margin-bottom: 20px;
+    border-radius: 5px;
+  }
+  .editorContent {
+    width: 100%;
+  }
+  .options {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+}
+</style>
