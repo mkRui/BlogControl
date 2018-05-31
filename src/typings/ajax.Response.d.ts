d48323136041ed6a595a143253bdebb1331e0737
shicongrui <1102163949@qq.com>
2018-05-31 08:09:30 +0800

    ajax 设置返回强类型规定

--- src/typings/ajax.Response.d.ts
+++ src/typings/ajax.Response.d.ts
@@ -1,11 +1,17 @@
 declare namespace ajaxRes {
+
+  // axios 默认
   interface VueRes {
     data: reState
   }
-
   interface reState {
+    // 后台自定义状态码
     code: number,
+
+    // 后台返回信息
     message: string,
+
+    // 后台返回的数据
     result: any
   }
 }
